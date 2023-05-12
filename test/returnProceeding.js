eee4658f505da860722cfbbb372a64287ed92531
Viktor Stenquist <viktor.stenquist@live.se>
2023-05-12 23:12:08 +0200

    added wait/until-s. and removed some sleep's

--- test/returnProceeding.js
+++ test/returnProceeding.js
@@ -2,7 +2,7 @@ const {Builder, By, Key, until} = require ('selenium-webdriver');
 const should = require('chai').should();
 
 /* As a customer, i would like to be able to reorder an allready orderd order */
-describe('Create a reorder', () => {
+describe.only('Create a reorder', () => {
     //Test case:
     context('Filling the form', () => {
         it('IDK?', async () => {
@@ -15,11 +15,11 @@ describe('Create a reorder', () => {
                 await driver.get('https://magento.softwaretestingboard.com/');
 
                 //Access orders and return page
-                await driver.wait(until.elementLocated(By.css('.nav:nth-child(4) a')), 10000);
+                await driver.wait(until.elementLocated(By.css('.nav:nth-child(4) a')), 30000);
                 await driver.findElement(By.css('.nav:nth-child(4) a')).click();
                 
                 //Fill Order-ID
-                await driver.wait(until.elementLocated(By.css('#oar-order-id')), 10000);
+                await driver.wait(until.elementLocated(By.css('#oar-order-id')), 30000);
                 await driver.findElement(By.id('oar-order-id')).sendKeys('000007642 ');
 
                 //Fill Billing-lastname
@@ -33,38 +33,37 @@ describe('Create a reorder', () => {
                 //Fill Email
                 await driver.findElement(By.id('oar_email')).sendKeys('viktor.stenquist@live.se');
 
-                //Wait for 2 extra seconds
+                //Let whe website load for 2 seconds extra
                 await driver.sleep(2000);
 
                 //Press: continue-button
+                await driver.wait(until.elementLocated(By.className('primary')), 30000);
                 await driver.findElement(By.className('primary')).click();
 
-                 //Wait for 2 extra seconds
-                 await driver.sleep(2000);
+                //Let whe website load for 2 seconds extra
+                await driver.sleep(2000);
 
                 //Press on the reorder link
-                await driver.wait(until.elementLocated(By.className('order')), 10000);
+                await driver.wait(until.elementLocated(By.className('order')), 30000);
                 await driver.findElement(By.className('order')).click();
 
-                 //Wait for 2 extra seconds
-                 await driver.sleep(2000);
+                //Let whe website load for 2 seconds extra
+                await driver.sleep(2000);
 
                 //Press: proceed-to-checkout-button
-                await driver.wait(until.elementLocated(By.css('.checkout-methods-items > .item > button')),10000);
+                await driver.wait(until.elementLocated(By.css('.checkout-methods-items > .item > button')),30000);
                 await driver.findElement(By.css('.checkout-methods-items > .item > button')).click();
 
-                //Wait for 2 extra seconds
+                //Let whe website load for 2 seconds extra
                 await driver.sleep(2000);
 
                 //Fill new form
                 //Fill email-address
-                await driver.wait(until.elementLocated(By.id('customer-email')), 10000);
+                await driver.wait(until.elementLocated(By.id('customer-email')), 30000);
                 await driver.findElement(By.id('customer-email')).sendKeys('viktor.stenquist@live.se');
 
-                //Wait for 2 extra seconds
-                await driver.sleep(2000);
-
                 //Fill first-name
+                await driver.wait(until.elementLocated(By.name('firstname')), 30000);
                 await driver.findElement(By.name('firstname')).sendKeys('viktor');
                 
                 //Fill last-name
@@ -75,6 +74,9 @@ describe('Create a reorder', () => {
 
                 //Fill City
                 await driver.findElement(By.name('city')).sendKeys('Öckerö');
+
+                //Let whe website load for 1 second extra
+                await driver.sleep(1000);
 
                 //Pick Sweden as country
                 const selectContry = await driver.findElement(By.name('country_id'));
@@ -89,28 +91,19 @@ describe('Create a reorder', () => {
                 //fill nummber
                 await driver.findElement(By.name('telephone')).sendKeys('072 757 32 32');
 
-                //Wait for 10 extra seconds
-                await driver.sleep(10000);
-
-
+                //Let whe website load for 5 seconds extra
+                await driver.sleep(5000);
 
                 // Press "NEXT" button
+                await driver.wait(until.elementLocated(By.css('#shipping-method-buttons-container > .primary > button')), 30000);
                 await driver.findElement(By.css('#shipping-method-buttons-container > .primary > button')).click();
 
-                //Wait for 10 extra seconds
+                //Let whe website load for 2 seconds extra
                 await driver.sleep(10000);
 
                 // Press "PLACE ORDER" button
+                await driver.wait(until.elementLocated(By.css('.actions-toolbar > .primary > .checkout')), 30000);
                 await driver.findElement(By.css('.actions-toolbar > .primary > .checkout')).click();
-                
-
-                
-
-
-                
-                
-                
-
             }
             finally {
                 //await driver.quit();
