4691625284e93064025dd3ef9b8d0889e1021251
Tim Jonsson <4yourztruly@gmail.com>
2023-05-17 10:10:19 +0200

    tims commit

---
+++ test/createloginLuma.js
@@ -0,0 +1,62 @@
+
+const { link } = require('fs');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+const should = require('chai').should();
+
+async function googleSearch() {
+    
+    let driver = await new Builder().forBrowser('firefox').build();
+
+    try {
+       
+        await driver.get('https://magento.softwaretestingboard.com/customer/account/create/');
+        
+
+        let firstButton = await driver.wait(until.elementLocated(By.name('firstname')), 10000);
+        
+        await firstButton.click();
+
+        await driver.findElement(By.name('firstname')).sendKeys('Tim', Key.RETURN);
+
+        let secondButton = await driver.wait(until.elementLocated(By.name('lastname')), 10000);
+
+        await secondButton.click();
+
+        await driver.findElement(By.name('lastname')).sendKeys('Jonsson', Key.RETURN);
+
+        let thirdButton = await driver.wait(until.elementLocated(By.name('email')), 10000);
+
+        await thirdButton.click();
+
+        await driver.findElement(By.name('email')).sendKeys('blahblah@gmail.com', Key.RETURN);
+
+        let fourthButton = await driver.wait(until.elementLocated(By.name('password')), 10000);
+
+        await fourthButton.click();
+
+        await driver.findElement(By.name('password')).sendKeys('Blah1!1#2!L', Key.RETURN);
+
+        let fifthButton = await driver.wait(until.elementLocated(By.name('password_confirmation')), 10000);
+
+        await fifthButton.click();
+
+        await driver.findElement(By.name('password_confirmation')).sendKeys('Blah1!1#2!L', Key.RETURN);
+
+
+
+    
+        
+    
+
+      
+
+    } catch(error) {
+        console.log(error);
+    } finally {
+        //console.log('Test ran successfully.');
+        //await driver.quit();
+    }
+}
+
+googleSearch();
---
+++ test/loginLuma.js
@@ -0,0 +1,45 @@
+
+const { link } = require('fs');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+const should = require('chai').should();
+
+async function googleSearch() {
+    
+    let driver = await new Builder().forBrowser('firefox').build();
+
+    try {
+       
+        await driver.get('https://magento.softwaretestingboard.com/customer/account/login/');
+        
+
+        let firstButton = await driver.wait(until.elementLocated(By.name('login[username]')), 10000);
+        
+        await firstButton.click();
+
+        await driver.findElement(By.name('login[username]')).sendKeys('blahblah@gmail.com', Key.RETURN);
+
+        let secondButton = await driver.wait(until.elementLocated(By.name('login[password]')), 10000);
+
+        await secondButton.click();
+
+        await driver.findElement(By.name('login[password]')).sendKeys('Blah1!1#2!L', Key.RETURN);
+
+
+
+      
+    
+        
+    
+
+      
+
+    } catch(error) {
+        console.log(error);
+    } finally {
+        //console.log('Test ran successfully.');
+        //await driver.quit();
+    }
+}
+
+googleSearch();
