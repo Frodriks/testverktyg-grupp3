ab28c3a2a060d2b03ea28092ff71169a4d7ba995
IsaiahLeon <isaiah.leon.eriksson@gmail.com>
2023-05-21 12:18:35 +0200

    söndag 1218

--- test/newCreateLogin.js
+++ test/newCreateLogin.js
@@ -1,21 +1,16 @@
-
-/*const { Builder, By, Key, until } = require("selenium-webdriver");
-
+const { Builder, By, Key, until } = require("selenium-webdriver");
 const should = require("chai").should();
 
-
-let driver = new Builder().forBrowser("firefox").build();
-
-describe.skip("Skapa ett konto", () => {
+//Som kund vill jag kunna skapa ett konto för att kunna ha tillgång till personliga sidor.
+describe("Skapa ett konto", () => {
   context("Fyll i personliga detaljer för att skapa konto", () => { 
     it("Kontot är skapat och jag har tillgång till personliga sidor", async () => { 
+    let driver = new Builder().forBrowser("firefox").build();
       try {
-
         //hämta sidan
         await driver.get(
           "https://magento.softwaretestingboard.com/customer/account/create/"
         );
-
         // hitta elementet för förnamn
         let firstButton = await driver.wait(
           until.elementLocated(By.name("firstname")),
@@ -34,18 +29,14 @@ describe.skip("Skapa ett konto", () => {
         );
             //klicka knappen
         await secondButton.click();
-
         await driver
           .findElement(By.name("lastname"))
           .sendKeys("Jonsson", Key.RETURN);
-
         let thirdButton = await driver.wait(
           until.elementLocated(By.name("email")),
           10000
         );
-
         await thirdButton.click();
-
         await driver
           .findElement(By.name("email"))
           .sendKeys("blah123blah@gmail.com", Key.RETURN);
@@ -54,39 +45,26 @@ describe.skip("Skapa ett konto", () => {
           until.elementLocated(By.name("password")),
           10000
         );
-
         await fourthButton.click();
-
         await driver
           .findElement(By.name("password"))
           .sendKeys("Blah1!1#2!L", Key.RETURN);
-
         let fifthButton = await driver.wait(
           until.elementLocated(By.name("password_confirmation")),
           10000
         );
-
         await fifthButton.click();
-
         await driver
           .findElement(By.name("password_confirmation"))
           .sendKeys("Blah1!1#2!L", Key.RETURN);
-
-
           await driver.sleep(10000);
-
           //assertions
-
           await driver.wait(until.elementLocated(By.css('.logged-in')), 10000);
           const information = await driver.findElement(By.css('.logged-in')).getText();
-
           information.should.contain('Tim Jonsson')
-
           //testet funkar och man skapar ett konto men man behöver ändra vissa detaljer för att göra testet igen eftersom att man kan inte ha samma detaljer på flera konton
-
       } catch (error) {
         console.log(error);
-
       } finally {
         
         await driver.quit();
@@ -94,5 +72,3 @@ describe.skip("Skapa ett konto", () => {
     });
   });
 });
-
-*/
--- test/newLogin.js
+++ test/newLogin.js
@@ -1,13 +1,11 @@
-/*const { assert } = require("chai");
+const { assert } = require("chai");
 const { Builder, By, Key, until } = require("selenium-webdriver");
-
 const should = require("chai").should();
-
+//Som kund vill jag kunna logga in på sidan så att jag kan se mina sidor.
 describe("Logga in på kontot", () => {
   context("Fyll i konto detaljer och logga in", () => {
     it("Jag är inloggad och kan se personliga detaljer", async () => {
       let driver = await new Builder().forBrowser("firefox").build();
-
       try {
         await driver.get(
           "https://magento.softwaretestingboard.com/customer/account/login/"
@@ -22,36 +20,27 @@ describe("Logga in på kontot", () => {
             //skriv och klicka enter
         await driver
           .findElement(By.name("login[username]"))
-          .sendKeys("asdf@asdf.se", Key.RETURN);
-
+          .sendKeys("hejan@hejsan.hejsan", Key.RETURN);
         let secondButton = await driver.wait(
           until.elementLocated(By.name("login[password]")),
           10000
         );
-
         await secondButton.click();
-
         await driver
           .findElement(By.name("login[password]"))
-          .sendKeys("Thisisapassword1", Key.RETURN);
-
+          .sendKeys("LoggaIn123", Key.RETURN);
           await driver.sleep(10000);
-
           await driver.wait(until.elementLocated(By.css('.logged-in')), 10000);
           const information = await driver.findElement(By.css('.logged-in')).getText();
-
           information.should.contain('Test Testsson')
-
         // testet funkar inte eftersom en recaptcha behövs att fyllas i (antibot) men den fyller i detaljerna som den ska och jag har en assert som jag bara kan anta att den funkar
             //testet körs genom att skriva "npx mocha --no-timeouts"
       } catch (error) {
         console.log(error);
       } finally {
         await driver.quit();
-
         
       }
     });
   });
 });
-*/
